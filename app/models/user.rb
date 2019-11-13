e51800414b2345c3a8e348deef70efb37e1805c2
program.sakura <program.sakura@gmail.com>
2019-11-13 22:36:26 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favposts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -29,6 +31,22 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  # お気に入りの追加
+  def like(micropost)
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  # お気に入りの削除
+  def unlike(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  # お気に入りの登録判定
+  def favpost?(micropost)
+    self.favposts.include?(micropost)
   end
   
 end
